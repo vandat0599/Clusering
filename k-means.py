dad2b56723e12e76f0bb7555c1b3ef4bb000ca48
Đạt <vovandat28051999@gmail.com>
2019-12-03 03:18:15 +0700

    done k-means

--- k-means.py
+++ k-means.py
@@ -1,7 +1,148 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math as m
-from sklearn.cluster import KMeans
+# from sklearn.cluster import KMeans
+
+
+
+def euclidianDistance(x, y):
+    return m.sqrt(m.pow(abs(x[0] - y[0]),2) + m.pow(abs(x[1] - y[1]),2))
+
+class k_means:
+    def __init__(self, k=2, thresold = 0.001, max_iter = 300, has_converged=False):
+    
+        ''' 
+        Class constructor
+        
+        Parameters
+        ----------
+        - k: number of clusters. 
+        - thresold (percentage): stop algorithm when difference between prev cluster 
+                                and new cluster is less than thresold
+        - max_iter: number of times centroids will move
+        - has_converged: to check if the algorithm stop or not
+        '''
+
+
+        self.k = k
+        self.thresold = thresold
+        self.max_iter = max_iter
+        self.has_converged= has_converged
+        
+    def initCentroids(self, X):
+        ''' 
+        Parameters
+        ----------
+        X: input data. 
+        '''
+        self.centroids=[]
+        
+        #Starting clusters will be random members from X set
+        indexes = np.random.randint(0, len(X)-1,3)
+        self.centroids=X[indexes]
+            
+        
+    def updateCentroids(self, cur_centroids):
+        '''
+        Class constructor
+        
+        Parameters
+        ----------
+        cur_centroids: list of new centroids
+        
+        '''
+        self.has_converged=True
+        
+        for c in range(0,self.k):
+            prev_centroid = self.centroids[c]
+            cur_centroid  = cur_centroids[c]
+            #checking if % of difference between old position and new position is more than thresold
+            
+            #TODO d=?
+
+            d = euclidianDistance(prev_centroid,cur_centroid)
+            if  d > self.thresold:
+                self.has_converged = False
+                self.centroids = cur_centroids
+            
+    def fit(self, X):
+        '''
+        FIT function, used to find clusters
+    
+        Parameters
+        ----------
+        X: input data. 
+        '''
+        #Init list cluster centroids
+        self.initCentroids(X)
+            
+        #Main loop
+        for i in range(self.max_iter):  
+            #Centroids for this iteration
+            cur_centroids = []
+            
+            for centroid in range(0,self.k):
+                #List samples of current cluster
+                samples = []
+                
+                for k in range(len(X)):
+                    d_list = []
+                    for j in range(self.k):
+                        d_list.append(euclidianDistance(self.centroids[j], X[k]))
+                    
+                    # Cluster has minimal distance between its centroid and data sample
+                    # TODO (c=???)
+                    c = d_list.index(min(d_list))
+
+                    #Store sample to list
+                    if c == centroid:
+                        samples.append(X[k]) 
+                
+                #New centroids of each cluster is calculated by mean of all samples closest to it
+                new_centroid = [0,0]
+                new_centroid[0] = np.mean([x[0] for x in samples])
+                new_centroid[1] = np.mean([x[1] for x in samples])
+                #TODO (new_centroid=???)
+
+            
+                cur_centroids.append(new_centroid)
+                
+            self.updateCentroids(cur_centroids)
+
+
+            
+            if self.has_converged:
+                break
+        
+        #Each cluster represented by its centroid
+        return np.array(self.centroids)
+
+    def predict(self, data):
+        ''' 
+        Parameters
+        ----------
+        data: input data.
+        
+        Returns:
+        ----------
+        pred: list cluster indexes of input data 
+        '''
+    
+        pred = []
+        for i in range(len(data)):
+            # Create list distances between centroids and data sample
+            d_list = []
+            for j in range(len(self.centroids)):
+                # Calculate distances between current data sample and centroid(using euclidian distance) 
+                # Store to d_list
+                #TODO 
+                d_list.append(euclidianDistance(self.centroids[j], data[i]))
+
+            # Store the Cluster has minimal distance between its centroid and current data sample to pred
+            #TODO 
+            pred.append(d_list.index(min(d_list)))
+            
+        return np.array(pred)
 
 def visualize(X, label):
     K = np.amax(label) + 1
@@ -35,10 +176,17 @@ def main():
     original_label = np.asarray([0]*N + [1]*N + [2]*N).T
 
     # test result KMeans lib
-    model2 = KMeans(n_clusters=3, random_state=0).fit(X)
-    print('Centers found by scikit-learn:')
-    print(model2.cluster_centers_)
-    pred_label = model2.predict(X)
-    visualize(X, pred_label)
+    # model2 = KMeans(n_clusters=3, random_state=0).fit(X)
+    # print('Centers found by scikit-learn:')
+    # print(model2.cluster_centers_)
+    # pred_label = model2.predict(X)
+    # visualize(X, pred_label)
+
+    # test result k-means class
+    model1=k_means(k=3)
+    print('Centers found by your model:')
+    print(model1.fit(X))
+    pred=model1.predict(X)
+    visualize(X,pred)
 main()
  
